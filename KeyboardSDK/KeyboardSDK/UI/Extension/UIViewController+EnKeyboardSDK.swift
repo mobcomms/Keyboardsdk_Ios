dc9f73229233fb91a426f85256096d1b68a57f90
Son JaeMin <>
2025-04-01 12:51:11 +0900

    xcode 업데이트에 따른 링크 이동 로직 최신화

--- KeyboardSDK/KeyboardSDK/UI/Extension/UIViewController+EnKeyboardSDK.swift
+++ KeyboardSDK/KeyboardSDK/UI/Extension/UIViewController+EnKeyboardSDK.swift
@@ -22,38 +22,83 @@ public extension UIViewController {
         self.present(vc, animated: true, completion: nil)        
     }
     
-    /// Open url inside app extension
-    ///
-    /// - Parameter url: url
-    func open(url: URL) {
-        var responder: UIResponder? = self as UIResponder
-        let selector = #selector(openURL(_:))
-        while responder != nil {
-            if responder! is UIApplication {
-                responder!.perform(selector, with: url)
-                return
-            }
-            responder = responder?.next
-        }
-    }
-    
-    /// Fake selector to suppress compile error
-    ///
-    /// - Parameter url: useless
-    /// - Returns: useless
-//    @objc func openURL(_ url: URL) -> Bool {
-//        return true
+//    /// Open url inside app extension
+//    ///
+//    /// - Parameter url: url
+//    func open(url: URL) {
+//        var responder: UIResponder? = self as UIResponder
+//        let selector = #selector(openURL(_:))
+//        while responder != nil {
+//            if responder! is UIApplication {
+//                responder!.perform(selector, with: url)
+//                return
+//            }
+//            responder = responder?.next
+//        }
 //    }
-    @objc func openURL(_ url: URL) -> Bool {
+//    
+//    /// Fake selector to suppress compile error
+//    ///
+//    /// - Parameter url: useless
+//    /// - Returns: useless
+////    @objc func openURL(_ url: URL) -> Bool {
+////        return true
+////    }
+//    @objc func openURL(_ url: URL) -> Bool {
+//        var responder: UIResponder? = self
+//        while responder != nil {
+//            if let application = responder as? UIApplication {
+//                return application.perform(#selector(openURL(_:)), with: url) != nil
+//            }
+//            responder = responder?.next
+//        }
+//        return false
+//    }
+    
+    @objc func open(url: URL) -> Bool {weak
+
         var responder: UIResponder? = self
+        print("0331 open :: \(url)")
         while responder != nil {
             if let application = responder as? UIApplication {
-                return application.perform(#selector(openURL(_:)), with: url) != nil
+                print("0331 open :: application : \(application)")
+
+                if application.canOpenURL(url) {
+                    print("0331 open :: canOpenURL")
+
+                    application.open(url, options: [:], completionHandler: nil)
+                    return true
+                } else {
+                    print("0331 open :: canOpenURL false!!!")
+
+                    return false
+                }
             }
             responder = responder?.next
         }
         return false
     }
+
+    
+    @objc func openURL(_ url: URL) -> Bool {weak
+        var responder: UIResponder? = self
+        print("0331 openURL :: \(url)")
+
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                if application.canOpenURL(url) {
+
+                    application.open(url, options: [:], completionHandler: nil)
+                    return true
+                } else {
+                    return false
+                }
+            }
+            responder = responder?.next
+        }
+        return false
+    }
+
 }
 
 
